3899cb2d05a1ee5f6695d4f2f3b2fd5a4f7eab32
Andy Fang <andy@fang.ws>
2014-10-27 16:21:52 -0400

    commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,9 @@ module.exports = function(grunt) {
 		sass: {
 			dist: {
 				files: {
-					'galaxy.css' : 'galaxy.scss'
+					'galaxy.css' : 'galaxy.scss',
+					'css/docs.css' : 'css/docs.scss',
+					'css/home.css' : 'css/home.scss'
 				}
 			}
 		},
