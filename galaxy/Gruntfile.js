8028c5256b769811e8f264f2fc54b59420acdb71
Andy Fang <andy@fang.ws>
2014-11-01 10:55:27 -0400

    new

---
+++ galaxy/Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				files: {
+					'galaxy.css' : 'galaxy.scss',
+					'css/docs.css' : 'css/docs.scss',
+					'css/home.css' : 'css/home.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['watch']);
+}
